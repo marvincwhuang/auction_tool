21a319a1e9b1f7a11559544bf179a879a4582e95
Chiwei Huang <cwhuang0523@gmail.com>
2019-11-22 21:19:05 +0800

    implement add/remove row

--- app/javascript/packs/templates/form.js
+++ app/javascript/packs/templates/form.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   let buyMoreItemsCount = document.querySelectorAll(".buy_more_item").length
-  let ImagesCount = document.querySelectorAll(".image_url").length
-
+  let ImagesCount = document.querySelectorAll(".image_item").length
   const createTemplatePage = document.getElementById('createTemplatePage')
   if (!createTemplatePage) {
     return null
@@ -19,61 +18,66 @@ window.onload = function () {
   addBuyMoreItemButton.addEventListener('click', (e) => {
     console.log('addBuyMoreItemButton')
     e.preventDefault()
+    addBuyMoreItem(buyMoreItemsCount)
     buyMoreItemsCount += 1
-    setBuyMoreItem(buyMoreItemsCount)
   })
   removeBuyMoreItemButton.addEventListener('click', (e) => {
     e.preventDefault()
     if (buyMoreItemsCount > 0) {
+      removeBuyMoreItem()
       buyMoreItemsCount -= 1
     }
-    setBuyMoreItem(buyMoreItemsCount)
+
   })
 
   const addImageButton = document.getElementById('addImageButton')
   const removeImageButton = document.getElementById('removeImageButton')
   addImageButton.addEventListener('click', (e) => {
     e.preventDefault()
+    addImageItem(ImagesCount)
     ImagesCount += 1
-    setImageItem(ImagesCount)
   })
   removeImageButton.addEventListener('click', (e) => {
     e.preventDefault()
     if (ImagesCount > 0) {
+      removeImageItem(ImagesCount)
       ImagesCount -= 1
     }
-    setImageItem(ImagesCount)
   })
 }
 
-function setBuyMoreItem(count) {
+function addBuyMoreItem(currentCount) {
   const buyMoreItemsArea = document.getElementById('buyMoreItemsArea')
-  let content = ''
-  for (i = 0; i < count; i++) {
-    content = content + `
-    <div class="form-group col-md-3">
-      <input class="form-control" type="text" name="buy_more_items[]" placeholder="商品${i+1}">
-    </div>
-    <div class="form-group col-md-9">
-      <input class="form-control" type="text" name="buy_more_item_urls[]" placeholder="商品網址${i+1}">
-    </div>
-    `
-  }
-  buyMoreItemsArea.innerHTML = content
-  return null
+  let node = document.createElement("div");
+  node.className = 'buy_more_item'
+  node.innerHTML = `<div class="form-group col-md-3">
+    <input class="form-control" type="text" name="buy_more_items[]" placeholder="商品${currentCount+1}">
+  </div>
+  <div class="form-group col-md-9">
+    <input class="form-control" type="text" name="buy_more_item_urls[]" placeholder="商品網址${currentCount+1}">
+  </div>`
+  buyMoreItemsArea.appendChild(node)
 }
 
-function setImageItem(count) {
+function removeBuyMoreItem() {
+  const buyMoreItemsArea = document.getElementById('buyMoreItemsArea')
+  buyMoreItemsArea.removeChild(buyMoreItemsArea.lastElementChild)
+}
+
+function addImageItem(currentCount) {
   const ImagesArea = document.getElementById('ImagesArea')
-  let content = ''
-  for (i = 0; i < count; i++) {
-    content = content + `
-    <div class="form-group col-md-12">
-      <label for="image-${i+1}">圖片${i+1}</label>
-      <input class="form-control" type="text" name="image_urls[]" placeholder="圖片網址${i+1}">
-    </div>
-    `
-  }
-  ImagesArea.innerHTML = content
-  return null
+  let node = document.createElement("div");
+  node.className = 'image_item'
+  node.innerHTML = `
+  <div class="form-group col-md-12">
+    <label for="image-${currentCount+1}">圖片${currentCount+1}</label>
+    <input class="form-control" type="text" name="image_urls[]" placeholder="圖片網址${currentCount+1}">
+  </div>
+  `
+  ImagesArea.appendChild(node)
+}
+
+function removeImageItem() {
+  const ImagesArea = document.getElementById('ImagesArea')
+  ImagesArea.removeChild(ImagesArea.lastElementChild)
 }
