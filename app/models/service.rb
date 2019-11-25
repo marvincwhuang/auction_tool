abe4087be4bc92d48fcea8b10ec5ef3dd74062c6
Chiwei Huang <cwhuang0523@gmail.com>
2019-11-25 23:27:44 +0800

    allow same service name

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   has_many :prices, :dependent => :destroy
   belongs_to :brand
   has_many :products_services
